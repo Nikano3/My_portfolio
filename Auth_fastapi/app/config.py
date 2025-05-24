2a77a042e1e01bc0f4cf2e6c4a69b6e6e24c96ec
Nikano <nikano.live@mail.ru>
2025-05-24 20:26:12 +0300

    12345

--- Auth_fastapi/app/config.py
+++ Auth_fastapi/app/config.py
@@ -1,16 +1,18 @@
 from pydantic_settings import BaseSettings, SettingsConfigDict
+import os
 
 
 class Settings(BaseSettings):
-    model_config = SettingsConfigDict(env_file=".env")
+    model_config = SettingsConfigDict(
+        env_file=os.path.abspath(os.path.join(os.path.dirname(__file__), "../.env"))
+    )
+
     POSTGRES_USER: str
     POSTGRES_PASSWORD: str
     POSTGRES_SERVER: str
     POSTGRES_DB: str
     JWT_SECRET: str
-    JWT_LIFETIME: int  # лучше int, если это число в секундах
-
-
+    JWT_LIFETIME: int
 
 
 settings = Settings()
